0a3d25c03efeeb33444816b53f50bbf42ebf6e1b
LimaNat <160279258+LimaNat@users.noreply.github.com>
2024-10-28 12:36:08 +0000

    Add usuario_service

--- app/repositories/usuario_repositories.py
+++ app/repositories/usuario_repositories.py
@@ -1,9 +1,9 @@
 from models.usuario_model import Usuario
 from sqlalchemy.orm import Session
 
-class usuariorepository:
-    def __init__(sef, session: Session):
-    self.session = session
+class UsuarioRepository:
+    def __init__(self, session: Session):
+        self.session = session
 
 def salvar_usuario(self, usuario: Usuario):
     self.session.add(usuario)
---
+++ app/services/usuario_service.py
@@ -0,0 +1,26 @@
+from models.usuario_model import Usuario
+from repositories.usuario_repositories import UsuarioRepository
+
+class usuarioService:
+
+    def __init__(self, repository: UsuarioRepository):
+        self.repository = repository 
+
+    def criar_usuario(self, nome: str, email: str, senha: str):
+        try:
+            usuario = Usuario(nome = nome, email = email, senha = senha)
+
+            novo_usuario = self.repository.pesquisar_usuario_por_email(usuario.email)
+
+            if novo_usuario:
+                print("Usuário já cadastrado!")
+
+                self.repository.salvar_usuario(usuario)
+                print("Usuário cadastrado com sucesso!")
+            except TypeError as erro:
+                print(f"Erro ao salvar o usuario: {erro}")
+            except Exception as erro:
+                print(f"Ocorreu um erro inesperado: {erro}")
+            
+        def listar_todos_usuarios(self):
+            return self.repository.listar_usuario()
